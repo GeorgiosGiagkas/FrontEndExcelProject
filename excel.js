f96886d5de3d444b16552ff90307b63ef42fdee4
Giagkas Georgios <geogiagkas@gmail.com>
2019-09-14 16:55:54 +0300

    [Step 11]: Add Event Listener =>Total Profit Calculation

--- excel.js
+++ excel.js
@@ -1,23 +1,34 @@
-console.log("running");
+// console.log("running");
 
 let unitCol = document.querySelector("#units");
-let salesCol= document.querySelector("#sales");
+// let salesCol= document.querySelector("#sales");
+// let priceCol = document.querySelector("#prices");
+// let profitCol = document.querySelector("#profits");
+// let totalProfitsCol = document.querySelector("#total-profits");
+// unitCol.style.backgroundColor = "red";
+// unitCol.style.border = "2px solid";
 
-unitCol.style.backgroundColor = "red";
-unitCol.style.border = "2px solid";
-//Change content
-unitCol.textContent = salesCol.textContent * 10;
+unitCol.addEventListener("input",handler);
 
-
-
-try{
-    run();
+//hoesting
+function handler(e){
+    let profit = e.target.previousElementSibling;
+    let totalProfit = e.target.nextElementSibling.nextElementSibling;
+    totalProfit.textContent =  parseInt(e.target.textContent) *   parseInt(profit.textContent);
 }
-catch(error){
-    console.log(error, error.message)
-}
 
-console.log("run code");
+
+
+
+// try{
+//     run();
+// }
+// catch(error){
+//     console.log(error, error.message)
+// }
+
+// console.log("run code");
+
 
 
 
